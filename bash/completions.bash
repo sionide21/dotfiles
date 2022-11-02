4c4afa50501a7541eb346961f70b6a367aaad229
Ben Olive <ben.olive@salesloft.com>
2022-11-02 14:59:34 -0400

    Faster `gt` tab completion
    At 200 projects, this was taking almost a second.

--- bash/completions.bash
+++ bash/completions.bash
@@ -1,7 +1,7 @@
 _dirs() {
     local dir
     dir=`basename $1`
-    find -L $1 -type d -maxdepth 1 | xargs  -II basename I | grep -v $dir
+    find -L $1 -type d -maxdepth 1 | perl -pe 's/.*\/([^\/]+)$/$1/' | grep -v $dir
 }
 
 _list_projects() {
