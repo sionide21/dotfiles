252035c2ae120a31805523a448e7d4c1c6a90bf5
Ben Olive <ben.olive@salesloft.com>
2023-10-03 11:21:25 -0400

    Some bash stuff

--- dotfiles/bashrc.bash
+++ dotfiles/bashrc.bash
@@ -17,3 +17,19 @@ export EDITOR="vim"
 alias pp="pbpaste | pipewrench"
 alias pw="pipewrench"
 alias ve="vacuform exec"
+
+# Enable globstar (`**` expands subdirectories)
+shopt -s globstar
+
+# Compile docs when installing erlang
+export KERL_BUILD_DOCS=yes
+
+# Use binstubs if available
+# Install binstubs using the following command
+#
+#     bundle install --binstubs=.git/safe/bin
+#
+# Since we're adding a relative path to our search path, tell bash to check
+# if the file still exists instead of trusting its internal cache
+shopt -s checkhash
+export PATH=".git/safe/bin:$PATH"
